ef1b8e0a77db64e58eeaeb762dc9dafb38edcae2
sepiggy <jms1209@qq.com>
2017-09-06 14:50:25 +0800

    4-12 completed

---
+++ src/main/java/cn/sepiggy/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package cn.sepiggy.dao;
+
+import cn.sepiggy.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/cn/sepiggy/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package cn.sepiggy.dao;
+
+import cn.sepiggy.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
